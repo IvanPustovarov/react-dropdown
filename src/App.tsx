422c77e3a4a86b0068fa2c50a56fc347ae423b4b
John <silverflash150@gmail.com>
2023-01-19 22:41:20 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { DropDown } from "./components/DropDown";
+
+export function App() {
+    return (
+        <div>
+            <DropDown />
+        </div>
+    )
+}
